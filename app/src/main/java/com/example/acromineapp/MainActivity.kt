55986466cabde0893164c9b053b23c9e671c1de7
Shaik, Jani <JaniShaik@quickenloans.com>
2021-12-05 13:12:15 -0500

    Adding search ui
    Initial commit

---
+++ app/src/main/java/com/example/acromineapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.acromineapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // below line is to get our inflater
+        val inflater = menuInflater
+
+        // inside inflater we are inflating our menu file.
+        inflater.inflate(R.menu.menu_item, menu)
+
+        // below line is to get our menu item.
+        val searchItem: MenuItem = menu.findItem(R.id.actionSearch)
+
+        // getting search view of our item.
+        val searchView: SearchView = searchItem.getActionView() as SearchView
+
+        // below line is to call set on query text listener method.
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // inside on query text change method we are
+                // calling a method to filter our recycler view.
+                //filter(newText)
+                return false
+            }
+        })
+        return true
+    }
+
+}
